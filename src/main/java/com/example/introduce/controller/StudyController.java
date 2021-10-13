51db51267a7a4093f7d77a08b3dbd5ac0019fca2
Jaeun-Choi98 <wodns617@gmail.com>
2021-10-13 18:36:38 +0900

    second commit(수정)

--- src/main/java/com/example/introduce/controller/StudyController.java
+++ src/main/java/com/example/introduce/controller/StudyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller()
 public class StudyController {
 
-    @GetMapping("/study/home")
+    @GetMapping("/study-home")
     public String studyHome(){
         return "/study/study-home";
     }
