1077c9f9a68e332b2a57d700bdef74dbaaf20b08
Jaeun-Choi98 <wodns617@gmail.com>
2021-10-27 18:48:37 +0900

    third commit

--- src/main/java/com/example/introduce/controller/StudyController.java
+++ src/main/java/com/example/introduce/controller/StudyController.java
@@ -1,13 +1,52 @@
 package com.example.introduce.controller;
 
+import com.example.introduce.model.Board;
+import com.example.introduce.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller()
 public class StudyController {
 
+    @Autowired
+    BoardRepository boardRepository;
+
     @GetMapping("/study-home")
-    public String studyHome(){
+    public String studyHome(Model model){
+        List<Board> boards = boardRepository.findAll();
+        model.addAttribute("boards",boards);
         return "/study/study-home";
+    }
+
+    @GetMapping("/write-home")
+    public String goWrite(Model model, @RequestParam(required = false) Long id){
+        if(id==null){
+            model.addAttribute("board",new Board());
+        }else{
+            //글 수정
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board",board);
+        }
+        return "/study/write-home";
+    }
+
+    @PostMapping("/write-home")
+    public String submitWrite(@ModelAttribute Board board){
+        boardRepository.save(board);
+        return "redirect:/study-home";
+    }
+
+    @GetMapping("/look-home")
+    public String look(Model model, @RequestParam(required = false) Long id){
+        Board board = boardRepository.findById(id).orElse(null);
+        model.addAttribute("board",board);
+        return "/study/look-home";
     }
 }
---
+++ src/main/java/com/example/introduce/model/Board.java
@@ -0,0 +1,18 @@
+package com.example.introduce.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String topic;
+    private String content;
+}
